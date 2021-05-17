2bd41ee46e31180d188c5c3a186eee9f0436275c
SakshiV <68046089+sakshivandure@users.noreply.github.com>
2021-05-17 11:27:28 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClickComponent } from './click/click.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path:'click', component:ClickComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/click/click.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-click',
+  templateUrl: './click.component.html',
+  styleUrls: ['./click.component.scss']
+  
+})
+export class ClickComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
